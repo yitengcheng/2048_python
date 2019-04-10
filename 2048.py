eeb5f6ecd817a6b83842f307d9b2782794c112c2
fanzehua <316679581@qq.com>
2019-04-10 11:18:27 +0800

    '初始化项目'

---
+++ 2048.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File : main.py
+@Time : 2019/04/09 11:55:30
+@Author : FanZehua
+@Version : 1.0
+@Contact : 316679581@qq.com
+@License : (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc : None
+'''
+
+# here put the import lib
+import wx
